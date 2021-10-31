a555757341ca24334e318113d92a28468f783099
allenlu <f2212671555@gmail.com>
2021-10-31 15:17:47 +0800

    update yaml

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # nodes
-kubectl delete deployment clique-testnet
-kubectl delete service clique-testnet
+kubectl delete deployment bootnode
+kubectl delete service bootnode
 kubectl delete deployment clique-testnet-miner1
 kubectl delete deployment clique-testnet-miner2
 kubectl delete deployment clique-testnet-miner3
